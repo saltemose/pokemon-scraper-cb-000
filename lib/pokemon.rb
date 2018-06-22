2440d7212a7699c658d900063c2f3a609b2261a3
Sean Altemose <sean@seanaltemose.com>
2018-06-22 14:36:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,16 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
+  def initialize(db)
+    db.each do |attr, val|
+      if attr == "id:"
+        @id = val
+      elsif attr == "name:"
+        @name = val
+      else @type = val
+      end
+    end
+  end
+
 
 end

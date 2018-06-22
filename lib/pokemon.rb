a26dc3eff5ccf45d7ed87483179c9bf8ffbc85ce
Sean Altemose <sean@seanaltemose.com>
2018-06-22 14:22:03 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,18 +19,9 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
 
-  def self.find(id_num, db)
-   pokemon_data = db.execute("SELECT * FROM pokemon where id = ?", id_num)
-   pokemon_data.each do |attr, val|
-     if attr = "id"
-       id = val
-     elsif attr = "name"
-       name = val
-     else attr = "type"
-       type = val
-     end
-    return self.new(id: id, name: name, type: type, db: db)
-  end
+  def self.find(id, db)
+    pokemon_data = db.execute('SELECT * FROM pokemon WHERE id = ?;', id).flatten
+    self.new(id: pokemon_data[0], name: pokemon_data[1], type: pokemon_data[2], hp: pokemon_data[3], db: db)
   end
 
 end
